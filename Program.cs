03995ba2ad1bf26c60909060d5788195da10aa91
Bebra1-7 <grigorievn603@gmail.com>
2026-09-11 09:19:39 +0300

    Lab03: increment and decrement

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Банковский счёт");
+
+double balance = 1000;
+Console.WriteLine($"Начальный баланс: {balance}");
+
+balance += 500;
+Console.WriteLine($"После пополнения на 500: {balance}");
+
+balance -= 200;
+Console.WriteLine($"После покупки: {balance}");
+
+balance *= 1.05;
+Console.WriteLine($"После начисления: {balance}");
+
+balance /= 2;
+Console.WriteLine($"После деления: {balance}");
+
+Console.WriteLine("Постфикс vs префикс");
+
+int lnum = 1;
+Console.WriteLine($"lnum++: {lnum++}");
+Console.WriteLine($"after: {lnum}");
+
+int weeknum = 1;
+Console.WriteLine($"++weeknum: {++weeknum}");
+Console.WriteLine($"after: {weeknum}");
+
+Console.WriteLine($"");
+Console.WriteLine($"Практ. ловушка");
+
+int attempts = 0;
+Console.WriteLine($"Num {++attempts}");
+Console.WriteLine($"Num {++attempts}");
+Console.WriteLine($"Всего попыток: {attempts}");
